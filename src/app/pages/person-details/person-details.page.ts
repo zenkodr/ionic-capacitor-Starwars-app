6ece64d84811a7ed85a079ea6d2e169003270c71
Deirdre18 <deirdreweldon18@gmail.com>
2020-09-22 11:35:45 +0100

    updated person-details.page.ts

--- src/app/pages/person-details/person-details.page.ts
+++ src/app/pages/person-details/person-details.page.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
 import { EmailComposer } from '@ionic-native/email-composer/ngx';
 import { FavoriteService } from 'src/app/services/favorite.service';
+// IMPORT SOCIAL SHARING.
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-person-details',
@@ -14,10 +16,10 @@ export class PersonDetailsPage implements OnInit {
   person: any;
   isFavorite1 = false;
   personId = null;
- 
+   
   constructor(private activatedRoute: ActivatedRoute, private api: ApiService,
-    private emailComposer: EmailComposer, private favoriteService: FavoriteService) { }
- 
+    private emailComposer: EmailComposer, private favoriteService: FavoriteService, private socialSharing: SocialSharing) { }
+    
   ngOnInit() {
     this.personId = this.activatedRoute.snapshot.paramMap.get('id');
     this.api.getPerson(this.personId).subscribe(res => {
@@ -44,8 +46,8 @@ export class PersonDetailsPage implements OnInit {
   sharePerson() {
     let email = {
       to: "",
-      subject: `This person is cool!: ${this.person.name}`,
-      body: `What do you think? <br><br>`,
+      subject: `This person is cool, what do you think?: ${this.person.name}`,
+      body: `What do you think? Gender:${this.person.gender}<br>Eye Color:${this.person.gender}<br>Hair Color:${this.person.hair_color}<br>Height:${this.person.height}<br>Skin Color:${this.person.skin_color}<br>Birth Year:${this.person.birth_year}<br>Mass:${this.person.mass}`,
       isHtml: true
     };
 
