bf3afc3c76211d20363a9ca0bbfe373402342911
Deirdre18 <deirdreweldon18@gmail.com>
2020-09-23 02:32:10 +0100

    changes to person-details.page.html, person-details.page.ts

--- src/app/pages/person-details/person-details.page.ts
+++ src/app/pages/person-details/person-details.page.ts
@@ -13,14 +13,21 @@ import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 })
 export class PersonDetailsPage implements OnInit {
 
+  text: string='This person is cool!'
   person: any;
   isFavorite1 = false;
-  link = this.api.getPerson;
+  link:string='this.api.getPerson(this.personId)';
   personId = null;
  
    
   constructor(private activatedRoute: ActivatedRoute, private api: ApiService,
     private emailComposer: EmailComposer, private favoriteService: FavoriteService, private socialSharing: SocialSharing) { }
+
+    ShareGeneric(parameter){
+      const url = this.link
+      const text = parameter+'\n'
+      this.socialSharing.share(text, null,this.link)
+    }
     
     ngOnInit() {
       this.personId = this.activatedRoute.snapshot.paramMap.get('id');
@@ -44,71 +51,22 @@ export class PersonDetailsPage implements OnInit {
     this.favoriteService.unfavoritePerson(this.personId).then(() => {
       this.isFavorite1 = false;
     });
-  }
+  }  
   
-  sharePerson() {
-    let email = {
-      to: "",
-      subject: `This person is cool!: ${this.person.name}`,
-      body: `What do you think?<br>  
-
-      <ion-col col-12 col-sm>
-      Gender:</ion-col><br>
-
-      <ion-col col-12 col-sm>
-      ${this.person.gender}
-      </ion-col><br>
-
-      <ion-col col-12 col-sm>
-      Eye Color:
-      </ion-col>
-
-      <ion-col col-12 col-sm>
-      ${this.person.eye_color}
-      </ion-col><br>
-
-      <ion-col col-12 col-sm>
-      Hair Color:
-      </ion-col><br>
-
-      <ion-col col-12 col-sm>
-      ${this.person.hair_color}
-      </ion-col><br>      
-
-      <ion-col col-12 col-sm>
-      Skin Color:
-      </ion-col><br>
-
-      <ion-col col-12 col-sm>
-      ${this.person.skin_color}
-      </ion-col><br>
-
-      <ion-col col-12 col-sm>
-      <div>Height:</div>
-      </ion-col>
-
-      <ion-col col-12 col-sm>
-      <div>${this.person.height}</div>
-      </ion-col><br>
-
-      <ion-col col-12 col-sm>
-      Date of Birth:
-      </ion-col><br>
-
-      <ion-col col-12 col-sm>
-      ${this.person.birth_year}
-      </ion-col><br>
-
-      <ion-col col-12 col-sm>
-      Mass:
-      </ion-col>
-
-      <ion-col col-12 col-sm>
-      ${this.person.mass}</ion-col>'`,
-      isHtml: true
-    };
-  
-    this.emailComposer.open(email);
+  ShareFacebook(){
+    this.socialSharing.shareViaFacebookWithPasteMessageHint(this.text, this.link, null /* url */, 'Copy Paste!')
   }
-  
+
+  SendEmail(){
+    this.socialSharing.shareViaEmail(this.text, this.link, null /* url */, ['email@address.com'])
   }
+
+  SendTwitter(){
+    this.socialSharing.shareViaTwitter(this.text, this.link, null /* url */)
+  }
+
+  SendInstagram(){
+    this.socialSharing.shareViaInstagram(this.text, this.link)
+  }
+   
+}
